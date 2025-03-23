ff92778cf5d06f847e79df05917521a217f3da16
りょや <ryoya.i.1215@gmail.com>
2025-03-23 11:36:17 +0900

    A-Doors-in-the-Center.cppの出力形式を修正し、偶数のNに対して正しい改行を追加しました。

--- ABC398/A-Doors-in-the-Center.cpp
+++ ABC398/A-Doors-in-the-Center.cpp
@@ -6,17 +6,17 @@ int main(){
   int N;
   cin >> N;
 
-if(N % 2 == 0){
-  for(int i = 0; i < (N - 2) / 2; i++){
-    cout << "-";
+  if(N % 2 == 0){
+    for(int i = 0; i < (N - 2) / 2; i++){
+      cout << "-";
+    }
+    cout << "==";
+    for(int i = 0; i < (N - 2) / 2; i++){
+      cout << "-";
+    }
+    cout << endl;
+    return 0;
   }
-  cout << "==";
-  for(int i = 0; i < (N - 2) / 2; i++){
-    cout << "-";
-  }
-  cout << endl;
-  return 0;
-}
 
 
     for(int j = 0; j < N/2; j++){
