216ddd11f080a34bafe6998aca1d4ad4cd21285a
りょや <ryoya.i.1215@gmail.com>
2025-03-23 11:35:33 +0900

    新しいC++プログラムファイルを追加し、与えられた整数Nに基づいてドアの形状を表示する機能を実装しました。

---
+++ ABC398/A-Doors-in-the-Center.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  int N;
+  cin >> N;
+
+if(N % 2 == 0){
+  for(int i = 0; i < (N - 2) / 2; i++){
+    cout << "-";
+  }
+  cout << "==";
+  for(int i = 0; i < (N - 2) / 2; i++){
+    cout << "-";
+  }
+  cout << endl;
+  return 0;
+}
+
+
+    for(int j = 0; j < N/2; j++){
+      cout << "-" << "";
+    }
+    cout << "=" << "";
+    for(int j = 0; j < N/2; j++){
+      cout << "-" << "";
+    }
+}
