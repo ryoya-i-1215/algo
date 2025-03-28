bca0c2d225d09ecce1606872ea91f962826bfc96
りょや <ryoya.i.1215@gmail.com>
2025-03-28 21:23:21 +0900

    コードの可読性を向上させるために、コメントを追加しました。

--- ABC388/C-Various-Kagamimochi.cpp
+++ ABC388/C-Various-Kagamimochi.cpp
@@ -15,7 +15,9 @@ int main() {
 
     long long count = 0;
     for (int i = 0; i < N - 1; ++i) {
+        // 2分探索で、A[i] * 2以上の最初の要素を探す
         auto it = lower_bound(A.begin() + i + 1, A.end(), A[i] * 2);
+        // それ以降の要素の数をカウント
         count += A.end() - it;
     }
 
